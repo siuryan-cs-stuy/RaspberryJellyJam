7ae11b7365dbeb8256501b3f2e00da971187fc61
JamesSmith1202 <jsmith@stuy.edu>
2017-01-07 22:02:15 -0500

    Initial file commit with method signatures

---
+++ CSVArray.java
@@ -0,0 +1,15 @@
+/*
+The class CSV reads in a csv file, converting it into a 2d ArrayList of Objects for use in the CSVTool class
+ */
+
+import java.util.ArrayList;
+    
+public class CSVArray{
+    private Arraylist data;
+    
+    public CSV(String filename){
+    }
+
+    public static Object typePicker(String thing){
+    }
+}
---
+++ CSVTool.java
@@ -0,0 +1,128 @@
+
+
+
+public class CSVTool{
+    
+    /*=======================================================
+     Accessor Methods
+      =====================================================*/
+    public static void getCells(int x, int y){
+    }
+
+    public static void getRow(int row){
+    }
+
+    public static void getCol(int col){
+    }
+
+    /*=======================================================
+     Set Methods
+      =====================================================*/
+    public void setCell(int x, int y){
+    }
+
+    public void setRow(int row){
+    }
+
+    public void setCol(int col){
+    }
+
+    /*=======================================================
+      Add Methods
+      =====================================================*/
+    public void addCol(CSVArray col){
+    }
+
+    public void addCol(CSVArray col, int pos){
+    }
+
+    public void addRow(CSVArray row){
+    }
+
+    public void addRow(CSVArray row, int pos){
+    }
+    
+    /*=======================================================
+     Delete Methods
+      =====================================================*/
+    public void deleteCell(int x, int y){
+    }
+
+    public void deleteRow(int row){
+    }
+
+    public void deleteCol(int col){
+    }
+
+    /*=======================================================
+      Sort Methods
+      =====================================================*/
+    public void sortRows(int col){
+    }
+
+    public void sortCols(int row){
+    }
+
+    /*=======================================================
+      Join Methods
+      =====================================================*/
+    public void innerJoin(int col1, int col2){
+    }
+
+    public void outerJoin(int col1, int col2){
+    }
+
+    public static void search(Object target){
+    }
+
+    public static void searchCell(Object target){
+    }
+
+    /*=======================================================
+      Mathematical Operations
+      =====================================================*/
+    public static void add(int col){
+    }
+    
+    public static void add(int col1, int col2){
+    }
+
+    public static void subtract(int col1, int col2){
+    }
+
+    public static void multiply(int col1, int col2){
+    }
+
+    public static void divide(int col1, int col2){
+    }
+
+    /*=======================================================
+      Statistics Methods
+      =====================================================*/
+    public static void stat5NumSummary(int col){
+    }
+
+    public static void boxAndWhisker(int col){
+    }
+
+    public static void average(int col){
+    }
+
+    public static void standardDev(int col){
+    }
+
+    public static void correlation(Object function){
+    }
+
+    /*=======================================================
+      Pretty Printing
+      =====================================================*/
+    public static void prettyPrint(){
+    }
+    
+    /*=======================================================
+      Testing
+      =====================================================*/
+    public static void main(String[] args){
+    }
+}
