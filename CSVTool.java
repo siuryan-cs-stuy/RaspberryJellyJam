44f9e3dac5d122b5a64b574f092400325a04bf8b
Ryan Siu <rsiu@stuy.edu>
2017-01-12 09:53:55 -0500

    added detailed comments

--- CSVTool.java
+++ CSVTool.java
@@ -1,8 +1,26 @@
+/**
+ * The class CSVTool parses arguments from the command line to determine which
+ * method to use. The action is then performed on the CSV, and any information
+ * that results from the method is printed to the console.
+ */
+
 public class CSVTool{
 
-    public String help();
+    /**
+     * Returns a string message that details which flag to use for which method.
+     *
+     * @return  string containing help data
+     */
+    public String help() {
 
+    }
+
+    /**
+     * Runs the Terminal CSV Editor. Uses args to take in arguments from the
+     * terminal.
+     */
     public static void main(String[] args){
+	
     }
 
 }
