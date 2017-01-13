5aafe134a81aafe1aca7baa09603affb12fdd9a5
Ryan Siu <rsiu@stuy.edu>
2017-01-13 09:59:37 -0500

    added prettyprint, various bug fixes

--- CSVGeneral.java
+++ CSVGeneral.java
@@ -14,12 +14,12 @@ public class CSVGeneral{
         return csv._data.get(y-1).get(x-1);
     }
 
-    public static ArrayList getRow(int row, CSVArray csv){
+    public static ArrayList<Object> getRow(int row, CSVArray csv){
 	return csv._data.get(row-1);
     }
 
-    public static ArrayList getCol(int col, CSVArray csv){
-        ArrayList column = new ArrayList();
+    public static ArrayList<Object> getCol(int col, CSVArray csv){
+        ArrayList<Object> column = new ArrayList<Object>();
 	for (ArrayList<Object> row : csv._data){
 	    column.add(row.get(col-1));
 	}
@@ -96,5 +96,49 @@ public class CSVGeneral{
 	    row.remove(col-1);
 	}
 	return csv;
-    }  
+    }
+
+    /*=======================================================
+      Pretty print
+      =====================================================*/
+    public static String prettyPrint(CSVArray csv) {
+	String retStr = "";
+	int rowNum = 1;
+	int[] maxSizes = new int[csv._data.get(0).size()];
+	Integer numRows = ((Integer) csv._data.size()).toString().length();
+
+	for (int i = 0; i < maxSizes.length; i++) {
+	    maxSizes[i] = maxSize( getCol(i+1,csv) );
+	}
+	
+        for (ArrayList<Object> row : csv._data) {
+	    retStr += formatCell(rowNum,numRows);
+	    for (int i = 0; i < row.size(); i++) {
+		retStr += formatCell(row.get(i),maxSizes[i]);
+	    }
+	    retStr += "\n";
+	    rowNum++;
+	}
+	return retStr;
+    }
+
+    private static String formatCell(Object cell, int maxSize){
+	String retStr = cell.toString();
+	for (int i = 0; i < maxSize - cell.toString().length(); i++) {
+	    retStr += " ";
+	}
+	retStr += "|";
+	return retStr;
+    }
+
+    private static int maxSize(ArrayList col) {
+	int size = 0;
+	for (int i = 0; i < col.size(); i++) {
+	    int currentSize = col.get(i).toString().length();
+	    if (currentSize > size) {
+		size = currentSize;
+	    }
+	}
+	return size;
+    }
 }
---
+++ Woo.java
@@ -0,0 +1,153 @@
+/**
+ * The class CSVTool parses arguments from the command line to determine which
+ * method to use. The action is then performed on the CSV, and any information
+ * that results from the method is printed to the console.
+ */
+
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
+public class Woo{
+    
+
+    /**
+     * Returns a string message that details which flag to use for which method.
+     *
+     * @return  string containing help data
+     */
+    public static String help() {
+	String helpStr = "";
+	helpStr += "command : arguments\n";
+	helpStr += "--help : none\n";
+	helpStr += "--getCell : x , y\n";
+	helpStr += "--getRow : row #\n";
+	helpStr += "--getCol : column #\n";
+	helpStr += "--setCell : x , y , value\n";
+	helpStr += "--setRow : row # , filename\n";
+	helpStr += "--setCol : column position , filename\n";
+	helpStr += "--addRow : filename\n";
+	helpStr += "--addRow : filename ,  position\n";
+	helpStr += "--addCol : filename\n";
+	helpStr += "--addCol : filename , position\n";
+	helpStr += "--delCell : x , y ";
+	helpStr += "--delRow :  row #\n";
+	helpStr += "--delCol :  column #\n";
+	helpStr += "--prettyPrint : none";
+	return helpStr;
+    }
+
+	
+    /**
+     * Runs the Terminal CSV Editor. Uses args to take in arguments from the
+     * terminal.
+     */
+    public static void main(String[] args)throws FileNotFoundException{
+	boolean writeToFile = false;
+	// if there are arguments
+	if (args.length != 0) {
+	
+	    //checks for the write to file flag
+	    writeToFile = (args[args.length-1]).equals("-w");
+
+	    //checks for the case where --help is the first arg and not the filename
+	    if (args[0].equals("--help")){
+		System.out.println(help());
+		return;
+	    }
+	    
+	    // if a command is present
+	    CSVArray csv = new CSVArray(args[0]); //create CSVArray with filename
+	    if (args.length != 1) {
+		String command = args[1];
+	    
+		if (command.equals("--getCell")){
+		    System.out.println(CSVGeneral.getCell(Integer.parseInt(args[2]), Integer.parseInt(args[3]), csv));
+		    return;
+		}
+
+		if (command.equals("--getRow")){
+		    System.out.println(CSVGeneral.getRow(Integer.parseInt(args[2]), csv));
+		    return;
+		}
+
+		if (command.equals("--getCol")){
+		    System.out.println(CSVGeneral.getCol(Integer.parseInt(args[2]), csv));
+		    return;
+		}
+
+		if (command.equals("--setCell")){
+		    System.out.println(CSVGeneral.setCell(Integer.parseInt(args[2]), Integer.parseInt(args[3]), CSVArray.typePicker(args[4]), csv));
+		    return;
+		}
+
+		if (command.equals("--setRow")){
+		    CSVArray second = new CSVArray(args[3]);
+		    System.out.println(CSVGeneral.setRow(Integer.parseInt(args[2]), second._data.get(0), csv));
+		    return;
+		}
+	
+		if (command.equals("--setCol")){
+		    CSVArray second = new CSVArray(args[3]);
+		    ArrayList col = new ArrayList();
+		    for (ArrayList i: second._data){
+			col.add(i.get(0));
+		    }	
+		    System.out.println(CSVGeneral.setCol(Integer.parseInt(args[2]), col, csv));
+		    return;
+		}
+
+		if (command.equals("--addCol")){
+		    if (args.length == 5){
+			System.out.println(CSVGeneral.addCol(new CSVArray(args[2]), Integer.parseInt(args[4]), csv));
+		    }
+		    else{
+			System.out.println(CSVGeneral.addCol(new CSVArray(args[2]), csv));
+		    }
+		    return;
+		}
+	
+		if (command.equals("--addRow")){
+		    if (args.length == 5){
+			System.out.println(CSVGeneral.addCol(new CSVArray(args[2]), Integer.parseInt(args[4]), csv));
+		    }
+		    else{
+			System.out.println(CSVGeneral.addCol(new CSVArray(args[2]), csv));
+		    }
+		    return;
+		}
+
+		if (command.equals("--delCell")){
+		    System.out.println(CSVGeneral.deleteCell(Integer.parseInt(args[2]), Integer.parseInt(args[3]), csv));
+		    return;
+		}
+
+		if (command.equals("--delRow")){
+		    System.out.println(CSVGeneral.deleteRow(Integer.parseInt(args[2]), csv));
+		    return;
+		}
+
+		if (command.equals("--delCol")){
+		    System.out.println(CSVGeneral.deleteCol(Integer.parseInt(args[2]), csv));
+		    return;
+		}
+
+		if (command.equals("--prettyPrint")){
+		    System.out.println(CSVGeneral.prettyPrint(csv));
+		    return;
+		}
+	
+		System.out.println("Invalid argument(s). Please use the --help flag for a list of commands.");
+	    }
+
+	    else {
+		System.out.println(CSVGeneral.prettyPrint(csv));
+		return;
+	    }
+	}
+    
+	//no args -> print help
+	else {
+	    System.out.println( help() );
+	}
+    }
+}
