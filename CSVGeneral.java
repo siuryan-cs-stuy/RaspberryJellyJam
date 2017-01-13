e0303fed78dd58448b5707412a7373bc302e2a7d
JamesSmith1202 <jsmith@stuy.edu>
2017-01-13 01:56:32 -0500

    Debugged and fixed all CSVGeneral methods to work in compliance with user input

--- CSVGeneral.java
+++ CSVGeneral.java
@@ -11,25 +11,26 @@ public class CSVGeneral{
       =====================================================*/
     
     public static Object getCell(int x, int y, CSVArray csv){
-        return csv._data.get(x).get(y);
+        return csv._data.get(y-1).get(x-1);
     }
 
     public static ArrayList getRow(int row, CSVArray csv){
-	return csv._data.get(row);
+	return csv._data.get(row-1);
     }
 
     public static ArrayList getCol(int col, CSVArray csv){
         ArrayList column = new ArrayList();
 	for (ArrayList<Object> row : csv._data){
-	    column.add(row.get(col));
+	    column.add(row.get(col-1));
 	}
+	return column;
     }
 
     /*=======================================================
       Set Methods
       =====================================================*/
     public static CSVArray setCell(int x, int y, Object value, CSVArray csv){
-	csv._data.get(x).set(y,value);
+	csv._data.get(y-1).set(x-1,value);
 	return csv;
     }
 
@@ -66,12 +67,12 @@ public class CSVGeneral{
     }
 
     public static CSVArray addRow(CSVArray row, CSVArray csv){
-	csv._data.add(row);
+	csv._data.add(row._data);
 	return csv;
     }
 
     public static CSVArray addRow(CSVArray row, int pos, CSVArray csv){
-	csv._data.add(pos,row);
+	csv._data.add(pos,row._data);
 	return csv;
     }
     
@@ -79,20 +80,20 @@ public class CSVGeneral{
       Delete Methods
       =====================================================*/
     public static CSVArray deleteCell(int x, int y, CSVArray csv){
-	ArrayList temp = csv._data.get(y);
-	temp.set(null, x);
-	csv._data.set(temp, y);
+	ArrayList temp = csv._data.get(y-1);
+	temp.set(x-1, null);
+	csv._data.set(y-1, temp);
 	return csv;
     }
 
     public static CSVArray deleteRow(int row, CSVArray csv){
-	csv._data.remove(row);
+	csv._data.remove(row-1);
 	return csv;
     }
 
     public static CSVArray deleteCol(int col, CSVArray csv){
 	for (ArrayList<Object> row : csv._data){
-	    row.remove(col);
+	    row.remove(col-1);
 	}
 	return csv;
     }  
